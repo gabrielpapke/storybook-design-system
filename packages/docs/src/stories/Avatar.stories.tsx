421c5e719b7cf924a6b17f60340f8c644f6639b8
Gabriel Papke Paganini <gabrielpapkepaganini@192.168.0.7>
2024-02-08 06:58:43 -0300

    feat: component controls

--- packages/docs/src/stories/Avatar.stories.tsx
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -8,6 +8,13 @@ export default {
     src: 'https://github.com/gabrielpapke.png',
     alt: 'Gabriel Papke',
   },
+  argTypes: {
+    src: {
+      control: {
+        type: 'text',
+      },
+    },
+  },
 } as Meta<AvatarProps>
 
 export const Primary: StoryObj<AvatarProps> = {}
